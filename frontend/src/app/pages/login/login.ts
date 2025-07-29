62a6eea1c175906df8ad7408e3b14d8ba759448e
carvalhxlucas <carvalhxlucas@gmail.com>
2025-07-29 16:33:55 -0300

    refactor: mudança visual na tela de login

--- frontend/src/app/pages/login/login.ts
+++ frontend/src/app/pages/login/login.ts
@@ -4,10 +4,24 @@ import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth';
 
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [
+    CommonModule, 
+    FormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule
+  ],
   templateUrl: './login.html',
   styleUrl: './login.scss'
 })
