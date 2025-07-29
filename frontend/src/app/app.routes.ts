8b7eac276c381f5434d25cdcb799cce79a7f5aac
carvalhxlucas <carvalhxlucas@gmail.com>
2025-07-29 11:45:40 -0300

    fix: corrigindo nome de arquivos dos services para facilitar legibilidade e manutenção

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomePage } from './pages/home-page/home-page';
-import { LoginComponent } from './pages/login/login';
-import { RegisterComponent } from './pages/register/register';
+import { LoginComponent } from './pages/login/login.service';
+import { RegisterComponent } from './pages/register/register.service';
 
 export const routes: Routes = [
   {
